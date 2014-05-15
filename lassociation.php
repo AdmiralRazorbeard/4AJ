9ab44590dd9d08c65ff1f52075d86be2b806c946
WFrancois <francois.wroblewski@epsi.fr>
2014-05-15 14:52:47 +0200

    Added lassociation memento and design

---
+++ lassociation.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+	<head>
+			<title>Présentation de l'association</title>
+			<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+			<link rel="stylesheet" href="design.css" />
+	</head> 
+	<body>
+		<div id="association">
+			<h1>L'association</h1>
+			<p>
+				L'association "4AJ, un tremplin pour les jeunes", a été créer le 7 juin 2010, suite à la fusion des 3 F.J.T. arragesois : Anne Frank, Clair Logis et Nobel.<br />
+				Plus de 20 000 jeunes ont été accueillis et logés depuis un demi-siècle dans les 3 établissements.<br />
+				Cette fusion a part ailleurs la mutualisation des moyens, un partage des compétences, et la mise en oeuvre d'actions communes.<br />
+				Cette association est active dans les parcours des usagers, des jeunes. Elle agit dans le parcours de soins à travers une organisation de coopération. <br />
+				Un FJT est une structure qui propose un logement peu onéreux à des jeunes, afin que ce soit un tremplin pour une insertion sociale et professionnelle, la mission première est l'insertion sociale et professionnelle des jeunes par le logement. <br />
+				Dans le Pas-de-Calais, les FJT sont atypiques car 40% des jeunes accueillis sont des jeunes confiés dans le cadre de l'aide sociale à l'enfance. <br />
+				Aujourd'hui ce sont 160 places dans les trois établissements dont 39 places de centre d'hébergement en réinsertion sociale (ou CHRS), 33 places dans le cadre de l'aide sociale à l'enfance, 5 places d'urgence, et 8 places dans le cadre de l'implication dans la campagne hivernale. L'originalité, la transversalité des dispositifs, la non stigmatisation du public accueilli sont mises en exergue par l'accueil d'une jeunesse large âgée de 16 à 30 ans.
+			</p>	
+		</div>
+		<div id="FJTAnneFrank">
+			<h1>FJT Anne Frank</h1>
+			<p>
+				
+			</p>
+		</div>
+		<div id="FJTClairLogis">
+			<h1>FJT Clair Logis</h1>
+			<p>
+				
+			</p>
+		</div>
+		<div id="FJTNobel">
+			<h1>FJT Nobel</h1>
+			<p>
+
+			</p>
+		</div>
+	</body>
+</html>
