b9157f3cf9a5e6b4018eefd2f9b008f6536783ca
Thomas Fournet <t.fournet062@gmail.com>
2014-05-15 17:16:17 +0200

    New informations1

---
+++ LiensUtiles.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+			<title>Liens Utiles</title>
+			<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+			<link rel="stylesheet" href="design.css" />
+	</head> 
+	<body>
+		Pour A.P.L. (Aide Personnalisée au Logement) c'est par ici: http://www.caf.fr/aides-et-services/les-services-en-ligne/acceder-a-une-demande-en-ligne
+
+		Differents types d'aides pourront être trouvée sur le site d'action logement: http://www.actionlogement.fr/
+		AIDE MOBILI-JEUNE® -> Aide destinée à faciliter l'accès à un logement meublé pour les jeunes de moins de 30 ans prenant ou reprenant un emploi nécessitant une mobilité. Droit ouvert sous certaines conditions, mis en place par Action Logement.
+		AVANCE LOCA-PASS® -> Avance du dépôt de garantie pour aider à l'accès au logement locatif. Droit ouvert mis en place par Action Logement
+	</body>
+</html>
