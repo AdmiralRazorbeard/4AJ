49534aa3ac0deb434f5ee3a818b34fa11c950c0a
WFrancois <francois.wroblewski@epsi.fr>
2014-05-19 11:06:45 +0200

    Update file to have the right link without .htaccess

--- admin/index.php
+++ admin/index.php
@@ -16,7 +16,7 @@ if(!empty($_SESSION['log']) && $_SESSION['log'] == 1 && !empty($_SESSION['mail']
 }
 if(!$isSuperAdmin)
 {
-	header('location:../index.html');
+	header('location:../index.php');
 }
 else
 {
