1ec2a24869851971b2e3f59a0c8aadfea07b83c3
Thomas Fournet <t.fournet062@gmail.com>
2014-05-19 15:24:38 +0200

    Beginning of css

--- view/includes/header.php
+++ view/includes/header.php
@@ -2,20 +2,36 @@
 <html>
 	<head>
 		<title>4AJ, un tremplin pour les jeunes</title>
-		<link rel="stylesheet" type="text/css" href="styles/style.css" />
+		<link rel="stylesheet" type="text/css" href="../style.css" />
 		<link rel="icon" type="image/png" href="../graphicRessources/favicon.jpg" >
 		<meta charset="utf-8">
 	</head> 
 	<body>
 	<div class="mainWrapper">
-		<div class="banniere"><img src="styles/tarot.jpg"></div>
 		<nav>
 			<ul>
-				<li><a href="index.php?page=association">L'association</a></li>
-				<li><a href="index.php?page=actualites">Actualités</a></li>
-				<li><a href="index.php?page=liensUtiles">Liens utiles</a></li>
-				<li><a href="index.php?page=vieEnFoyer">Vie en foyer</a></li>
-				<li><a href="index.php?page=devenirResidants">Devenir résidant</a></li>
-				<li><a href="index.php?page=contact">Contact</a></li>
+				<li><a id="item1" href="index.php?page=association">L'association</a></li>
+				<li><a id="item2" href="index.php?page=actualites">Actualités</a></li>
+				<li><a id="item3" href="index.php?page=liensUtiles">Liens utiles</a></li>
+				<li><a id="item4" href="index.php?page=vieEnFoyer">Vie en foyer</a>
+				    <ul id="s_item4">
+      					<li><a href="#">Services</a></li>
+      					<li><a href="#">Repas</a></li>
+      					<li><a href="#">Livre d'or</a></li>
+    				</ul>
+    			</li>
+				<li><a id="item5" href="index.php?page=devenirResidant">Devenir résidant</a>
+				    <ul id="s_item5">
+      					<li><a href="#">Conditions</a></li>
+      					<li><a href="#">Logements</a></li>
+    				</ul>
+    			</li>
+				<li><a id="item6" href="index.php?page=contact">Contact</a>
+					<ul id="s_item6">
+      					<li><a href="#">FAQ</a></li>
+      					<li><a href="#">Mémento</a></li>
+      					<li><a href="#">Faire un don</a></li>
+    				</ul>
+    			</li>	
 			</ul>
 		</nav>
