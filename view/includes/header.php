293dfad4b383cc394079f3d9352ce2f99145d84d
Thomas Fournet <t.fournet062@gmail.com>
2014-05-19 11:34:53 +0200

    delete used content

--- view/includes/header.php
+++ view/includes/header.php
@@ -6,3 +6,16 @@
 		<link rel="icon" type="image/png" href="../graphicRessources/favicon.jpg" >
 		<meta charset="utf-8">
 	</head> 
+	<body>
+	<div class="mainWrapper">
+		<div class="banniere"><img src="styles/tarot.jpg"></div>
+		<nav>
+			<ul>
+				<li><a href="index.php?page=association">L'association</a></li>
+				<li><a href="index.php?page=actualites">Actualités</a></li>
+				<li><a href="index.php?page=liensUtiles">Liens utiles</a></li>
+				<li><a href="index.php?page=vieEnFoyer">Vie en foyer</a></li>
+				<li><a href="index.php?page=devenirResidants">Devenir résidant</a></li>
+				<li><a href="index.php?page=contact">Contact</a></li>
+			</ul>
+		</nav>
