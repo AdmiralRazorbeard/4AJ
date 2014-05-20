a6ee6b9bfd7b2deb7f12640f85d0b350f005edd5
Thomas Fournet <t.fournet062@gmail.com>
2014-05-20 17:03:43 +0200

    Work on css 3

--- view/liensUtiles/liensUtiles.php
+++ view/liensUtiles/liensUtiles.php
@@ -1,18 +1,40 @@
 <?php include_once '../includes/header.php'; ?>
 	<body>
-		<div>
-			<p>Pour l'A.P.L. (Aide Personnalisée au Logement) c'est par ici: http://www.caf.fr/a
-
-			Differents types d'aides pourront être trouvée sur le site d'action logement: http://www.actionlogement.fr/
-			Entre autres:
-			AIDE MOBILI-JEUNE® -> Aide destinée à faciliter l'accès à un logement meublé pour les jeunes de moins de 30 ans prenant ou reprenant un emploi nécessitant une mobilité. Droit ouvert sous certaines conditions, mis en place par Action Logement.
-			AVANCE LOCA-PASS® -> Avance du dépôt de garantie pour aider à l'accès au logement locatif. Droit ouvert mis en place par Action Logement
-
-			Le site de l'union nationale pour l'habitat des jeunes -> http://www.unhaj.org/
-			Le site de l'Uriopss Nord-Pas-De-Calais -> http://www.uriopss-npdc.asso.fr/
-			Le site de la marie d'Arras -> http://www.arras.fr/
-			Le site de la Mission Locale en Pays d'Artois -> http://site.mlpa.fr/
-			</p>
-		</div>
+		<div class="contentWrapper">
+			<div class="association element">
+				<div class="liensUtiles element">
+					<h2>Pour l'A.P.L. (Aide Personnalisée au Logement)</h2>
+					<p>Pour l'A.P.L. (Aide Personnalisée au Logement) c'est par ici: http://www.caf.fr/</p>
+				</div>
+				<div class="liensUtiles element">
+					<h2>Differents types d'aides pourront être trouvée sur le site d'action logement</h2>
+					<p>http://www.actionlogement.fr/
+					Entre autres:
+					AIDE MOBILI-JEUNE® -> Aide destinée à faciliter l'accès à un logement meublé pour les jeunes de moins de 30 ans prenant ou reprenant un emploi nécessitant une mobilité. Droit ouvert sous certaines conditions, mis en place par Action Logement.
+					AVANCE LOCA-PASS® -> Avance du dépôt de garantie pour aider à l'accès au logement locatif. Droit ouvert mis en place par Action Logement
+					</p>
+				</div>
+				<div class="liensUtiles element">
+					<h2>Le site de l'union nationale pour l'habitat des jeunes</h2>
+					<p>http://www.unhaj.org/
+					</p>
+				</div>
+				<div class="liensUtiles element">
+					<h2>Le site de l'Uriopss Nord-Pas-De-Calais</h2>
+					<p>http://www.uriopss-npdc.asso.fr/
+					</p>	
+				</div>	
+				<div class="liensUtiles element">
+					<h2>Le site de la marie d'Arras</h2>
+					<p>http://www.arras.fr/
+					</p>	
+				</div>	
+				<div class="liensUtiles element">
+					<h2>Le site de la Mission Locale en Pays d'Artois</h2>
+					<p>http://site.mlpa.fr/
+					</p>	
+				</div>
+			</div>
+		</div>			
 	</body>
 </html>
