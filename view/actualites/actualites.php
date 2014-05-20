f0f6a9bd7bbce5c19fd82045a7ce49f45246f9ca
Thomas Fournet <t.fournet062@gmail.com>
2014-05-20 12:07:17 +0200

    actualites css

--- view/actualites/actualites.php
+++ view/actualites/actualites.php
@@ -1,1 +1,12 @@
 <?php include_once '../includes/header.php'; ?>
+	<body>
+		<div id="mainWrapper">
+			<div class="contentWrapper">			
+				<article class="actualites element">
+					<div class="a_title">Lorem ipsum dolor sit amet</div>
+					<div class="a_info">Publié le 20/05/2014 par <span class="bold">Thomas Fournet</span></div>
+					<div class="a_content">Duis non dui mi. Ut tempus lorem non velit eleifend, non porta mi rutrum. Sed accumsan arcu in risus congue, in laoreet velit auctor. Nulla eget dignissim turpis. Aliquam accumsan ultrices ipsum id ullamcorper. Praesent cursus dui sit amet magna ornare vulputate. Nulla facilisi. Phasellus rutrum mauris sapien, et auctor magna feugiat eget. </div>
+				</article>
+			</div>		
+	</body>
+</html>
