60bad322ba5a82413ed7c4b1b951ad5314b6a91a
Thomas Fournet <t.fournet062@gmail.com>
2014-05-21 09:14:07 +0200

    small changes

--- view/includes/header.php
+++ view/includes/header.php
@@ -19,26 +19,26 @@
 		<nav>
 			<ul>
 				<li><a id="item1" href="index.php?section=association">L'association</a></li>
-				<li><a id="item2" href="#">Actualités</a></li>
+				<li><a id="item2" href="index.php?section=actualites">Actualités</a></li>
 				<li><a id="item3" href="index.php?section=liensutiles">Liens utiles</a></li>
 				<li><a id="item4" href="index.php?section=vieenfoyer">Vie en foyer</a>
 				    <ul id="s_item4">
-      					<li><a href="#">Services</a></li>
-      					<li><a href="#">Repas</a></li>
+      					<li><a href="index.php?section=services">Services</a></li>
+      					<li><a href="index.php?section=repas">Repas</a></li>
       					<li><a class="last_item" href="index.php?section=livreor">Livre d'or</a></li>
     				</ul>
     			</li>
 				<li><a id="item5" href="index.php?section=devenirresidant">Devenir résidant</a>
 				    <ul id="s_item5">
-      					<li><a href="#">Conditions</a></li>
-      					<li><a class="last_item" href="#">Logements</a></li>
+      					<li><a href="index.php?section=conditions">Conditions</a></li>
+      					<li><a class="last_item" href="index.php?section=logements">Logements</a></li>
     				</ul>
     			</li>
 				<li><a id="item6" href="index.php?section=contact">Contact</a>
 					<ul id="s_item6">
-      					<li><a href="#">FAQ</a></li>
-      					<li><a href="#">Mémento</a></li>
-      					<li><a class="last_item" href="#">Faire un don</a></li>
+      					<li><a href="index.php?section=faq">FAQ</a></li>
+      					<li><a href="index.php?section=memento">Mémento</a></li>
+      					<li><a class="last_item" href="index.php?section=don">Faire un don</a></li>
     				</ul>
     			</li>	
 			</ul>
