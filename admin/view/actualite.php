c408d6f5d2568bd85fc90dc0ba5a8ab6f4614b89
Thomas Fournet <t.fournet062@gmail.com>
2014-05-28 10:19:16 +0200

    news style

--- admin/view/actualite.php
+++ admin/view/actualite.php
@@ -6,11 +6,11 @@ include_once '/view/includes/header.php';
 				<h1>Gestion des actualités </h1>
 				<fieldset id="actualite">
 				<form method="post">
-						<p class="form-field">
+						<div class="form-field">
 						<label for="titre">Titre :</label>
 						<input type="text" name="titre" id="titre" />
-						</p>
-						<p class="form-field">
+						</div>
+						<div class="form-field">
 						<label for="typeActualite">Type d'actualité : </label>
 						<select name="typeActualite" id="typeActualite"onchange="
 							if(this.selectedIndex == <?php echo $nbreTypeActualite; ?>)
@@ -24,22 +24,22 @@ include_once '/view/includes/header.php';
 								<!-- Redirige vers la page "new type actualité" -->
 								<option value="0">Ajouter un nouveau type d'actualité</option>
 						</select>
-						</p>
-						<p class="form-field-visibilite">
-						<label for="visiblePar">News visible par : </label><br>
-						<!-- Affiche toutes les autres "fonction" -->
-							<?php foreach ($allFonction as $k => $v) { ?>
-								<div class="form-field-visibilite-element">
-									<input type="checkbox" <?php if($k == 1) { echo 'checked'; } ?> name="<?php echo $k; ?>" id="<?php echo $k; ?>">
-									<label for="<?php echo $k; ?>"><?php echo $v['nom']; ?></label>
-								</div>
-				<?php       } ?>
-						</p>
-						<p class="form-field-contenu">
-						<label for="contenu">Contenu: </label>
+						</div>
+						<div class="form-field-visibilite">
+						<p>News visibles par :</p>
+							<!-- Affiche toutes les autres "fonction" -->
+								<?php foreach ($allFonction as $k => $v) { ?>
+									<div class="form-field-visibilite-element">
+										<input type="checkbox" <?php if($k == 1) { echo 'checked'; } ?> name="<?php echo $k; ?>" id="<?php echo $k; ?>">
+										<label for="<?php echo $k; ?>"><?php echo $v['nom']; ?></label>
+									</div>
+								<?php } ?>
+						</div>
+						<div class="form-field-contenu">
+						<p id="p_form-field-contenu">Contenu:</p>
 						<?php toolBox('contenu'); ?>
 						<input type="submit">
-						</p>
+						</div>
 					</form>
 				</fieldset>
 			</div>
