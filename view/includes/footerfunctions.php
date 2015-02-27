63836581523c2151f34f90859f989266ef238078
Thomas Fournet <t.fournet062@gmail.com>
2015-02-27 10:01:23 +0100

    last changes

--- view/includes/footerfunctions.php
+++ view/includes/footerfunctions.php
@@ -1,16 +1,14 @@
 <?php
 function nombreVisiteur()
 {
+	$tmp = run('SELECT nombre FROM nombrevisiteur WHERE id=1')->fetch_object();
+	$tmp = $tmp->nombre;
 	if(empty($_SESSION['visiteur']))
 	{
-		$tmp = run('SELECT nombre FROM nombrevisiteur WHERE id=1')->fetch_object();
-		$tmp = $tmp->nombre;
 		$tmp ++;
 		run('UPDATE nombrevisiteur SET nombre = '.$tmp);
 		$_SESSION['visiteur'] = true;
 	}
-	$tmp = run('SELECT nombre FROM nombrevisiteur WHERE id=1')->fetch_object();
-	$tmp = $tmp->nombre;
 	return $tmp;
 }
 ?>
